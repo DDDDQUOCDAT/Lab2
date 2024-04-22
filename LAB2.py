112d127186e694937f7041a50816f1c96ec9c874
Samuel <DDDD06.23@ichat.sp.edu.sg>
2024-04-22 13:35:02 +0800

    lab2 assignment files

--- LAB2.py
+++ LAB2.py
@@ -1,1 +1,32 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    user_input=get_user_input()
+    calc_average_temperature(user_input)
+    calc_min_max_temperature(user_input)
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+
+def get_user_input():
+    userinput=input("Enter numbers =" )
+    numlist=userinput.split(",")
+    new_list = [float(x) for x in numlist]
+    return new_list
+
+def calc_average_temperature(user_input):
+    average=sum(user_input)/len(user_input)
+    print("average =", average)
+    return average
+
+def calc_min_max_temperature(user_input):
+   min_num=min(user_input)
+   max_num=max(user_input)
+   print("max =", max_num)
+   print("min =", min_num)
+
+main()
+
+  
--- bmi.py
+++ bmi.py
@@ -16,4 +16,8 @@ def calculate_bmi(height, weight):
              print("normal Weight")
     
 calculate_bmi(weight=57, height=1.73)
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
 
