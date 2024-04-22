f4e39a7dd74fb1e4f6a5f0d74819d0617e5ed198
Samuel <DDDD06.23@ichat.sp.edu.sg>
2024-04-22 13:56:48 +0800

    Lab2 assignment

--- LAB2.py
+++ LAB2.py
@@ -1,10 +1,11 @@
-
+import statistics
 def display_main_menu():
     print("display_main_menu")
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
     user_input=get_user_input()
     calc_average_temperature(user_input)
     calc_min_max_temperature(user_input)
+    find_median_temperature (user_input)
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -26,6 +27,11 @@ def calc_min_max_temperature(user_input):
    max_num=max(user_input)
    print("max =", max_num)
    print("min =", min_num)
+
+def find_median_temperature (user_input):
+    sorted_input=sorted(user_input)
+    median=statistics.median(sorted_input)
+    print("median =", median)
 
 main()
 
