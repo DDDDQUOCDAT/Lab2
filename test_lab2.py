bba28a607d9431f072e6a04dd00d1d81ff48b2a1
Samuel <DDDD06.23@ichat.sp.edu.sg>
2024-04-30 17:20:01 +0800

    test_lab2.py

--- test_lab2.py
+++ test_lab2.py
@@ -1,9 +1,9 @@
-import lab2
+import lab2temperature
 
-def test_calc_min_max_temperature():
+def test_find_min_max():
     result=[]
-    user_input = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 ,8 , 9 , 10 ]
-    result = [int(min(user_input)), int(max(user_input))]
-    assert(result==lab2.calc_min_max_temperature(user_input))
+    user_input = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]
+    result = [min(user_input), max(user_input)]
+    assert( result == lab2temperature.calc_min_max_temperature(user_input) )
 
-
+    
